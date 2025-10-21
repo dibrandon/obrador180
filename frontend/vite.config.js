0a145ece3def537317577a27079103d56142191c
Alejandro pc2 <diazbrandonalejandro@gmail.com>
2025-10-21 18:02:10 +0200

    refactor(project): configurar backend como módulo ESM y ajustar frontend Vite con host dinámico y apertura automática

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,     // expone 127.0.0.1 y tu IP local correctamente
+    strictPort: false, // si 5173 está ocupado, usa 5174, 5175...
+    open: true // (no fijamos port, así no falla si está ocupado)
+    
+  }
 })
